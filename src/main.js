6fc6fd14a3a504fc7dc17660c7e05dcab36ef737
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2024-11-22 13:51:35 +0100

    add start screen

--- src/main.js
+++ src/main.js
@@ -1,32 +1,80 @@
 import './style.css'
 import Phaser from 'phaser'
 
-class GameScene extends Phaser.Scene {
+class MenuScene extends Phaser.Scene {
   constructor() {
-    super("scene-game"),
+    super("MenuScene"),
     this.windowWidth,
     this.windowHeight
   }
 
   preload() {
-    this.load.image("pleaseTurnYourDevice", "/assets/pleaseTurnYourDevice.png")
+    this.load.image("bg", "/assets/menu/bg.png")
+    this.load.image("planetOrange", "/assets/menu/planetOrange.png")
+    this.load.image("planetRed", "/assets/menu/planetRed.png")
+    this.load.image("planetEarth", "/assets/menu/planetEarth.png")
+    this.load.image("logo", "/assets/menu/logo.png")
+    this.load.image("playBtn", "/assets/menu/playBtn.png")
+    this.load.image("settingsBtn", "/assets/menu/settingsBtn.png")
+    this.load.image("shopBtn", "/assets/menu/shopBtn.png")
+
+    this.planetOrangeCoords = [canvasSizes.width * 0.65, canvasSizes.height * 0.7]
+    this.planetRedCoords = [canvasSizes.width * 0.9, canvasSizes.height * 0.2]
+    this.planetEarthCoords = [canvasSizes.width * 0.1, canvasSizes.height * 0.8]
+
+    this.planetOrangeSpeed = 0.2
+    this.planetRedSpeed = 0.35
+    this.planetEarthSpeed = 0.6
+
+    this.movePlanets = () => {
+      this.planetOrange.x += this.planetOrangeSpeed
+      this.planetRed.x += this.planetRedSpeed
+      this.planetEarth.x += this.planetEarthSpeed
+  
+      if (this.planetOrange.x > canvasSizes.width * 1.25)
+      {
+        this.planetOrange.x = -(canvasSizes.width * 0.25)
+      }
+      if (this.planetRed.x > canvasSizes.width * 1.25)
+      {
+        this.planetRed.x = -(canvasSizes.width * 0.25)
+      }
+      if (this.planetEarth.x > canvasSizes.width * 1.25)
+      {
+        this.planetEarth.x = -(canvasSizes.width * 0.25)
+      }
+    }
+
+    this.scaleButtons = buttonName => {
+      buttonName.setInteractive().on('pointerover', () => {
+        buttonName.setScale(zoom245x135 * 1.05);
+      })
+  
+      buttonName.setInteractive().on('pointerout', () => {
+        buttonName.setScale(zoom245x135);
+      })
+    }
   }
 
   create() {
-    this.pleaseTurnYourDevice = this.add.image(0, 0, "pleaseTurnYourDevice").setOrigin(0,0)
+    this.bg = this.add.image(0, 0, "bg").setOrigin(0,0).setScale(zoom245x135)
+    this.planetOrange = this.add.image(this.planetOrangeCoords[0], this.planetOrangeCoords[1], "planetOrange").setScale(zoom245x135)
+    this.planetRed = this.add.image(this.planetRedCoords[0], this.planetRedCoords[1], "planetRed").setScale(zoom245x135)
+    this.planetEarth = this.add.image(this.planetEarthCoords[0], this.planetEarthCoords[1], "planetEarth").setScale(zoom245x135)
+    this.logo = this.add.image(canvasSizes.width / 2, canvasSizes.height * 0.275, "logo").setScale(zoom245x135)
+    this.playBtn = this.add.image(canvasSizes.width / 2, canvasSizes.height * 0.575, "playBtn").setScale(zoom245x135)
+    this.settingsBtn = this.add.image(canvasSizes.width / 2, canvasSizes.height * 0.725, "settingsBtn").setScale(zoom245x135)
+    this.shopBtn = this.add.image(canvasSizes.width / 2, canvasSizes.height * 0.875, "shopBtn").setScale(zoom245x135)
+
+
   }
 
   update() {
-    this.windowWidth = window.innerWidth;
-    this.windowHeight = window.innerHeight;
+    this.movePlanets()
 
-    if (this.windowWidth <= this.windowHeight) {
-      this.pleaseTurnYourDevice.setScale(zoom140x77);
-      this.pleaseTurnYourDevice.visible = true;
-    }
-    else {
-      this.pleaseTurnYourDevice.visible = false;
-    }
+    this.scaleButtons(this.playBtn)
+    this.scaleButtons(this.settingsBtn)
+    this.scaleButtons(this.shopBtn)
   }
 }
 
@@ -35,7 +83,9 @@ const canvasSizes = {
   width: 3920
 }
 
-const zoom140x77 = 28;
+const zoom140x77 = 28
+const zoom245x135 = 16
+const fontSizeLarge = 100 
 
 const config = {
   type: Phaser.WEBGL,
@@ -43,7 +93,14 @@ const config = {
   height: canvasSizes.height,
   width: canvasSizes.width,
   pixelArt: true,
-  scene:[GameScene]
+  scene:[
+    MenuScene
+  ]
 }
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
+
+const switchScene = (oldScene, newScene) => {
+  console.log(`From ${oldScene.scene.key} to ${newScene}`);
+  oldScene.scene.start(newScene);
+};
