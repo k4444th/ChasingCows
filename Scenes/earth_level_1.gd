d07812a5979aabc341bd8b8ebd0cdc95ebf46574
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-03-29 20:34:35 +0100

    Add automatic ocean (border) to map

--- Scenes/earth_level_1.gd
+++ Scenes/earth_level_1.gd
@@ -1,6 +1,13 @@
 extends TileMap
 
 var borderAtlasCoords = Vector2i(0, 3)
+var oceanAtlasCoords = Vector2i(15, 2)
+var worldOrigin := Vector2i(0, 0)
+var oceanRadius := 0
+
+@export var oceanBorderWidth: int
+
+const PI = 3.1415926525
 
 enum layers {
 	layer0 = 0,
@@ -20,7 +27,31 @@ var offsets := [
 ]
 
 func _ready() -> void:
+	oceanRadius = findBoundaries()
 	setBoundaries()
+	placeOcean()
+
+func findBoundaries() -> int:
+	var maxPos = 0
+	var usedCells = get_used_cells(layers.layer1)
+	
+	for cell in usedCells:
+		if abs(cell.x) > maxPos or abs(cell.y) > maxPos:
+			maxPos = cell.x if cell.x >= cell.y else cell.y
+	
+	return maxPos + oceanBorderWidth
+
+func placeOcean():
+	for x in range(worldOrigin.x - oceanRadius, worldOrigin.x + oceanRadius):
+		for y in range(worldOrigin.y - oceanRadius, worldOrigin.y + oceanRadius):
+			var currentCell := Vector2i(x, y)
+			if isCellInCircle(currentCell, worldOrigin, oceanRadius):
+				set_cell(layers.layer0, currentCell, 0, oceanAtlasCoords)
+
+func isCellInCircle(cell: Vector2i, origin: Vector2i, radius: int) -> bool:
+	var dx := origin.x - cell.x
+	var dy := origin.y - cell.y
+	return pow(dx, 2) + pow(dy, 2) < pow(radius, 2)
 
 func setBoundaries():
 	var usedCells = get_used_cells(layers.layer1)
