837bd6d12fcb9a4e73aebb4445cad8551f5f5ef5
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-04-01 20:50:03 +0200

    add new Galaxy Navigation System

--- Scenes/choose_destination.gd
+++ Scenes/choose_destination.gd
@@ -1,10 +1,69 @@
 extends Node2D
 
+var selected: TextureButton
+
+@onready var selector := $Selector
+@onready var camera := $Camera
+
+@onready var andromedaButton := $Galaxies/Andromeda
+@onready var milkyWayButton := $Galaxies/MilkyWay
+@onready var hangayButton := $Galaxies/HangayButton
+@onready var fornaxButton := $Galaxies/FornaxButton
+@onready var triangulumButton := $Galaxies/TriangulumButton
+@onready var absanthaGomButton := $"Galaxies/Absantha-GomButton"
+@onready var absanthaShadButton := $"Galaxies/Absantha-ShadButtton"
+
+func _ready() -> void:
+	setSelector(andromedaButton)
+
 func _on_arrow_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
 func _on_arrow_left_pressed() -> void:
-	print("Arrow Left Pressed")
+	if selected == andromedaButton:
+		setSelector(absanthaShadButton)
+	elif selected == milkyWayButton:
+		setSelector(andromedaButton)
+	elif selected == hangayButton:
+		setSelector(milkyWayButton)
+	elif selected == fornaxButton:
+		setSelector(hangayButton)
+	elif selected == triangulumButton:
+		setSelector(fornaxButton)
+	elif selected == absanthaGomButton:
+		setSelector(triangulumButton)
+	elif selected == absanthaShadButton:
+		setSelector(absanthaGomButton)
+	else:
+		setSelector(andromedaButton)
 
 func _on_arrow_right_pressed() -> void:
-	print("Arrow Right Pressed")
+	if selected == andromedaButton:
+		setSelector(milkyWayButton)
+	elif selected == milkyWayButton:
+		setSelector(hangayButton)
+	elif selected == hangayButton:
+		setSelector(fornaxButton)
+	elif selected == fornaxButton:
+		setSelector(triangulumButton)
+	elif selected == triangulumButton:
+		setSelector(absanthaGomButton)
+	elif selected == absanthaGomButton:
+		setSelector(absanthaShadButton)
+	elif selected == absanthaShadButton:
+		setSelector(milkyWayButton)
+	else:
+		setSelector(andromedaButton)
+
+func setSelector(node: TextureButton):
+	selected = node
+	selector.selectorPosition = node.position + node.size * (node.scale / 2)
+	selector.selectorSize = node.size * node.scale
+	var tween = get_tree().create_tween()
+	tween.tween_property(camera, "position", node.position + node.size * (node.scale / 2), 2.5).set_trans(Tween.TRANS_QUAD)
+
+func _on_andromeda_pressed() -> void:
+	setSelector(andromedaButton)
+
+func _on_milky_way_pressed() -> void:
+	setSelector(milkyWayButton)
--- Scenes/selector.gd
+++ Scenes/selector.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var selectorPosition := Vector2(0, 0)
-var selectorSize := Vector2(100, 100)
+@export var selectorPosition := Vector2(0, 0)
+@export var selectorSize := Vector2(100, 100)
 
 @onready var selector := $"."
 @onready var selectorCorners := [
@@ -9,13 +9,12 @@ var selectorSize := Vector2(100, 100)
 	[$BottomLeft, $BottomRight]
 ]
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		selectorPosition = event.position
-	
-	if event is InputEventMouseButton:
-		selectorSize = Vector2(600 - selectorSize.x, 600 - selectorSize.y)
-		
+#func _input(event: InputEvent) -> void:
+	#if event is InputEventMouseMotion:
+		#selectorPosition = event.position
+	#
+	#if event is InputEventMouseButton:
+		#selectorSize = Vector2(600 - selectorSize.x, 600 - selectorSize.y)
 
 func _process(_delta: float) -> void:
 	selector.position = selectorPosition
