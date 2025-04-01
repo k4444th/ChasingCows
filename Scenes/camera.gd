a8363a4917be3d41d3760017ffc81653660db78a
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-04-01 17:57:08 +0200

    Better Navigation for Galaxy Menu

--- Scenes/camera.gd
+++ Scenes/camera.gd
@@ -8,14 +8,14 @@ var galaxyCount = 3
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_released("Right") and galaxyIndex < galaxyCount - 1:
-		var newPosition = position + Vector2(1920 * 2, 0)
+		galaxyIndex += 1
+		var newPosition = Vector2(1920 * 2 * galaxyIndex, 0)
 		var tween = get_tree().create_tween()
 		tween.tween_property(self, "position", newPosition, transitionSpeed).set_trans(Tween.TRANS_QUAD)
-		galaxyIndex += 1
 
 	elif Input.is_action_just_released("Left") and galaxyIndex > 0:
-		var newPosition = position - Vector2(1920 * 2, 0)
+		galaxyIndex -= 1
+		var newPosition = Vector2(1920 * 2 * galaxyIndex, 0)
 		var tween = get_tree().create_tween()
 		tween.tween_property(self, "position", newPosition, transitionSpeed).set_trans(Tween.TRANS_QUAD)
-		galaxyIndex -= 1
 	
